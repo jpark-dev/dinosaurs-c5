2f8af453462efb9415f2cb087f5c05b4a77dc33b
Junsung Park <zeipar@gmail.com>
2020-07-28 15:12:38 -0700

    Added header and addbutton components.

--- src/components/AttendeeForm.js
+++ src/components/AttendeeForm.js
@@ -1,14 +1,24 @@
 import React from "react";
 
 export default function AttendeeForm(props) {
+  console.log(props);
+  if (!props.isFormExist) {
+    return <></>;
+  }
+
   return (
-    <form onSubmit={event => props.handleSubmit(event)}>
+    <form onSubmit={e => props.handleSubmit(e)}>
       <label htmlFor='name'>Attendee Name: </label>
       <input type='text' name='name' />
       <label htmlFor='role'>Role: </label>
       <input type='text' name='role' />
       <label htmlFor='email'>Email: </label>
-      <input type='text' name='email'/>
+      <input type='text' name='email' />
+      <input
+        type='button'
+        value='cancel'
+        onClick={e => props.deleteForm(props.id)}
+      />
       <input type='hidden' name='id' value={props.id}></input>
       <input type='submit' />
     </form>
